85c08594f3c47c476c64810d1532359ca362cc35
Mark Harvey <mark990521@gmail.com>
2022-03-04 16:44:31 +0100

    close to done

--- JSlab2/quiz.js
+++ JSlab2/quiz.js
@@ -1,17 +1,105 @@
-let input = {}
+const questions = [
+    {
+        "Question": "What gender do you consider yourself?",
+        "Answer": "Male",
+        "DummyAnswers": ["Female", "Other"],
+        "Type": "radio"
+    },
+    {
+        "Question": "What/ which colours do you like?",
+        "Answer": ["Purple", "Green", "Yellow"],
+        "Type": "checkbox"
+    },
+    {
+        "Question": "What do you do as a pastime?",
+        "Answer": "",
+        "Type": "textarea"
+    },
+    {
+        "Question": "",
+        "Answer": "",
+        "Type": ""
+    },
+    {
+        "Question": "",
+        "Answer": "",
+        "Type": ""
+    }
+];
 
-function validate() {
 
+const loadQuestions = () => {
 
+    // alert(questions.length);
+
+    let BTNS = "";
+
+    for(let i = 0;i < questions.length; i++)
+    {
+        BTNS += `<button class="qbtn" onclick="OpenTab("${i+1}")">Q${i+1}</button>`;
+
+    };
+    
+    let QUES = "";
+
+    for(let i = 0; i < questions.length; i++)
+    {
+        QUES += 
+        `<div id="${i+1}" class="question" style="display:none;">
+        <h2>Question ${i+1}</h2>
+        <p>${questions[i].Question}</p>
+        </div>`;
+    };
+
+    writeHTML(BTNS, QUES);
 
 };
 
+const writeHTML = (buttons, questions) => {
+    const btns = document.querySelector('.questionButtons');
+    btns.innerHTML = buttons;
+    const div = document.querySelector('.questions');
+    div.innerHTML = questions;
+};
+
+function OpenTab(tab) {
+    const x = document.getElementsByClassName("questions");
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    };
+    document.getElementById(tab).style.display = "block";
+};
+
+function validate(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+    
+    const formJSON = Object.fromEntries(data.entries());
+
+    if (!(isName(formJSON.name))) {
+        alert(formJSON.name + ' is not a valid name');
+        return;
+    };
+
+    if (!(isEmail(formJSON.email))) {
+        alert(formJSON.email + ' is not a valid email');
+        return;
+    };
+
+  
+    
+    const results = document.querySelector('.results pre');
+    results.innerText = JSON.stringify(formJSON, null, 2);
+
+};
+
+const form = document.querySelector('.Quiz-Form');
+form.addEventListener('Submit', validate(this.onSubmit));
+
 function isName(name) {
     const letters = /^[A-Za-z]+$/;
-    if(name.value.match(letters)) {
-        return true;
-    };
-    return false;
+    return letters.test(String(name).toLowerCase());
 };
 
 function isEmail(email) {
