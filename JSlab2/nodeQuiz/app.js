60557aafe4d230db9f979db85688c453e5a7221a
Mark Harvey <mark990521@gmail.com>
2022-03-10 21:37:13 +0100

    nodeQuiz

---
+++ JSlab2/nodeQuiz/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const signale = require('signale');
+
+const PORT = process.env.PORT || 1337;
+
+const read = require('/routes/read');
+const pages = require('/routes/pages');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/read', read);
+app.use('/', pages);
+
+app.listen(PORT, () => {
+    signale.info('Server started and listening to port: ' + PORT);
+});
